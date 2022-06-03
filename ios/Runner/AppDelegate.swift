76cd3607c0335dc1be3e3c49d008d578495a95c0
Yang Chao <iyeatse@gmail.com>
2022-06-04 01:13:13 +0800

    add a cache object to simulate the memory used in business code

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,11 +4,15 @@ import Flutter
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate{
   
+  let cache = NSCache<NSString, NSData>()
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    
+    cache.setObject(Data(repeating: 1, count: 500 * 1024 * 1024) as NSData, forKey: "test")
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
